90584f9d29dcee6bcf2dd3837c3fa50599a21dc9
Ignacio Bonelli <ignacio@nachodigital.com.ar>
2024-07-31 22:01:24 -0300

    Adding GA4 to the site.

--- conf.py
+++ conf.py
@@ -1279,7 +1279,17 @@ COPY_SOURCES = False
 # Google Analytics or whatever else you use. Added to the bottom of <body>
 # in the default template (base.tmpl).
 # (translatable)
-# BODY_END = ""
+BODY_END = """
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-6YNZZZKVVS"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-6YNZZZKVVS');
+</script>
+"""
 
 # The possibility to extract metadata from the filename by using a
 # regular expression.
